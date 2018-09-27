58a81cfbd395b88483d3ae94cf0d2e6274f5a996
Terrance Rose Jr <trose618@gmail.com>
2018-09-27 20:03:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,1 @@
-function updateObjectWithKeyAndValue(obj, key, value){
-  
-  return Object.assign({}, obj, {[key]: value})
-}
-
-var meals = {}
-var sweetMeal = 'dessert'
-
-meals[sweetMeal] = 'cake'
-meals.snack = 'yogurt'
-
-console.log(meals.dessert)
-console.log(meals[sweetMeal])
-console.log("Keys and values in meal: \n" + meals)
+var playlist = { logic: "Bounce"}
